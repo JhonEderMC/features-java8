116352701b8a9bd8574a9b4a3b7532de7c44041b
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-23 23:06:26 -0500

    git create Reduce class

--- src/reduce/Reduce.java
+++ src/reduce/Reduce.java
@@ -1,2 +1,18 @@
-package reduce;public class Reduce {
+package reduce;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Reduce {
+
+    public static void sumNumbers() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        Integer result = numbers.stream().reduce(0, (partialResult, number)-> partialResult + number );
+        System.out.println("Result sum: "+ result);
+        result =0;
+        //Uses method reference
+        result = numbers.stream().reduce(0, Integer::sum);
+        System.out.println("Result sum: "+ result);
+    }
 }
