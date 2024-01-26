250e4223452fa06b0bd93f4026e04f381ec51a7d
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-26 18:37:00 -0500

    add refactoring to mejor good practices

--- src/reduce/Reduce.java
+++ src/reduce/Reduce.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Reduce {
 
-    public static void sumNumbers() {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    public static List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+    public static List<String> letters = Arrays.asList("a", "b", "c", "d", "e", "f");
 
+
+    public static void sumNumbers() {
         Integer result = numbers.stream().reduce(0, (partialResult, number)-> partialResult + number );
         System.out.println("Result sum: "+ result);
         result = 0;
@@ -17,8 +19,6 @@ public class Reduce {
     }
 
     public static void concatLetters() {
-        List<String> letters = Arrays.asList("a", "b", "c", "d", "e", "f");
-
         String concatLetters = letters.stream().reduce("", (partialSum, letter) -> partialSum + letter);
         System.out.println("Result sum letters: " + concatLetters);
         concatLetters = null;
@@ -32,6 +32,10 @@ public class Reduce {
         String upperCaseConcatLetters = letters.stream().reduce("", (partialResult, letter) -> partialResult + letter.toUpperCase());
         System.out.println(upperCaseConcatLetters);
         //We're not use method reference with toUpperCase this a local opertion to reference
+    }
+
+    public  static void sumNumbersParallel() {
+
     }
 
 
