87c832e1aa6e2b2987a988d8e3473e3949aac8da
jhonederMC <jhon.mosquera@sofka.com.co>
2024-02-08 21:15:27 -0500

    create infarce Car. This interface has static method. Implemente this intarface in Car class

---
+++ src/newinterface/Vehicule.java
@@ -0,0 +1,11 @@
+package newinterface;
+
+public interface Vehicule {
+    /*
+     The static producer() method is available only through and inside of an interface. it can't be overrriden by an
+     implementing class.
+     */
+    static String producer() {
+        return "N&F Vechicules";
+    }
+}
